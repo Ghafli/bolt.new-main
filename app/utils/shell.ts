acb8d627aef5c73380061a82c0997bbf1978d1d4
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-21 21:31:23 +0300

    Update shell.ts

--- app/utils/shell.ts
+++ app/utils/shell.ts
@@ -1,51 +1,36 @@
-import type { WebContainer } from '@webcontainer/api';
-import type { ITerminal } from '~/types/terminal';
-import { withResolvers } from './promises';
+import { WebContainer } from "@webcontainer/api";
+import { Files } from "@/app/lib/stores/files";
 
-export async function newShellProcess(webcontainer: WebContainer, terminal: ITerminal) {
-  const args: string[] = [];
+interface File {
+    name: string;
+    type: "file" | "directory";
+    children?: File[];
+}
 
-  // we spawn a JSH process with a fallback cols and rows in case the process is not attached yet to a visible terminal
-  const process = await webcontainer.spawn('/bin/jsh', ['--osc', ...args], {
-    terminal: {
-      cols: terminal.cols ?? 80,
-      rows: terminal.rows ?? 15,
-    },
-  });
 
-  const input = process.input.getWriter();
-  const output = process.output;
+export const getFilesFromWebContainer = async (webContainer: WebContainer): Promise<Files> => {
+    const files: Files = [];
 
-  const jshReady = withResolvers<void>();
+    const traverseDir = async (dirPath: string, currentFiles: File[]): Promise<void> => {
+        const entries = await webContainer.fs.readdir(dirPath, { withFileTypes: true });
 
-  let isInteractive = false;
-
-  output.pipeTo(
-    new WritableStream({
-      write(data) {
-        if (!isInteractive) {
-          const [, osc] = data.match(/\x1b\]654;([^\x07]+)\x07/) || [];
-
-          if (osc === 'interactive') {
-            // wait until we see the interactive OSC
-            isInteractive = true;
-
-            jshReady.resolve();
-          }
+        for (const entry of entries) {
+            const entryPath = `${dirPath}/${entry.name}`;
+            if (entry.isDirectory()) {
+                const newDir: File = {
+                    name: entry.name,
+                    type: "directory",
+                    children: []
+                };
+               await traverseDir(entryPath, newDir.children!);
+               currentFiles.push(newDir);
+            } else {
+                currentFiles.push({ name: entry.name, type: "file" });
+            }
         }
+    };
 
-        terminal.write(data);
-      },
-    }),
-  );
+    await traverseDir("/", files);
 
-  terminal.onData((data) => {
-    if (isInteractive) {
-      input.write(data);
-    }
-  });
-
-  await jshReady.promise;
-
-  return process;
+    return files;
 }
