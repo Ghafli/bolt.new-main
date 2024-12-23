d702ef2c2527b41a115898feb7db20f1b47b7b00
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-23 23:17:54 +0300

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,34 +1,17 @@
-// app/Root.tsx
-import React from "react";
-import {
-  Outlet,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
-import Header from "./components/header/Header";
-import { useAuth } from "./lib/stores/auth";
-import { LoadingDots } from "./components/ui/LoadingDots";
-
-
-const Root: React.FC = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const { user } = useAuth();
-
-
-  if(location.pathname !== "/" && !user){
-    navigate("/");
-     return <LoadingDots />;
-  }
-
-  return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <Header />
-      <div style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
-           <Outlet />
-      </div>
-    </div>
-  );
-};
-
-export default Root;
+--- a/app/Root.tsx
++++ b/app/Root.tsx
+@@ -1,10 +1,10 @@
+ import React from "react";
+ import {
+-  Outlet,
+-  useLocation,
+-  useNavigate,
++    Outlet,
++    useLocation,
++    useNavigate,
+ } from "react-router-dom";
+-import Header from "./components/header/Header";
++import Header from "@/app/components/header/Header";
+ import { useAuth } from "./lib/stores/auth";
+ import { LoadingDots } from "./components/ui/LoadingDots";
+ 
