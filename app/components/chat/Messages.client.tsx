991b9e28ed07030787ea73d40d54c493a697cbcb
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-23 23:41:10 +0300

    Update Messages.client.tsx

--- app/components/chat/Messages.client.tsx
+++ app/components/chat/Messages.client.tsx
@@ -1,24 +1,29 @@
 --- a/app/components/chat/Messages.client.tsx
 +++ b/app/components/chat/Messages.client.tsx
-@@ -18,15 +18,15 @@
-             }
-             const reader = response.body.getReader();
-                 while (true) {
--                const { done, value } = await reader.read();
--                if (done) {
--                    break;
--                }
-+                    const { done, value } = await reader.read();
-+                    if (done) {
-+                        break;
-+                    }
-                     const chunk = new TextDecoder().decode(value);
-                     partialResponse+=chunk;
+@@ -1,12 +1,14 @@
+ import React, { useEffect, useRef, useState } from "react";
+ import { useChat } from "@/app/lib/stores/chat";
+ import { AssistantMessage } from "./AssistantMessage";
+-import { UserMessage } from "./UserMessage";
++import { UserMessage } from "@/app/components/chat/UserMessage";
+ import { useSnapScroll } from "@/app/lib/hooks/useSnapScroll";
+ import styles from "./BaseChat.module.scss";
++import { useToast } from "@/app/lib/stores/toast";
+ 
+ const Messages: React.FC = () => {
+     const { messages, addMessage } = useChat();
++    const {showToast} = useToast();
+     const { snap } = useSnapScroll();
+ 
+     const chatBottomRef = useRef<HTMLDivElement>(null);
+@@ -27,8 +29,9 @@
                      addMessage({ type: "assistant", content: partialResponse });
--              }
--
-+                }
-+           
-             }
+               }
+ 
+-            }
++           }
           catch (error) {
++             showToast({message: "Error fetching stream response", type: "error"});
              console.error("Error fetching stream response:", error);
+          }
+          finally {
