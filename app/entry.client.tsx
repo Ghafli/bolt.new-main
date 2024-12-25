0959223f405e17cf4cce62d0e5b32217cbd46b9a
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-25 12:55:31 +0300

    Update entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,18 @@
-import { RemixBrowser } from '@remix-run/react';
-import { startTransition } from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import { RemixBrowser } from "@remix-run/react";
+import { startTransition } from "react";
+import { init } from "./lib/webcontainer";
 
-startTransition(() => {
-  hydrateRoot(document.getElementById('root')!, <RemixBrowser />);
-});
+init().then(() => {
+    startTransition(() => {
+        const root = document.getElementById("root")
+        if(root) {
+           const ssrCache = window.__remixContext
+           if(ssrCache) {
+             delete window.__remixContext
+           }
+            // @ts-expect-error
+           RemixBrowser.hydrateRoot(root, ssrCache)
+        }
+    })
+
+})
