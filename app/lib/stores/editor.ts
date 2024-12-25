fdaabfe9d3b69e9f761c3cffe77b2d6215a5be56
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-25 12:12:10 +0300

    Update editor.ts

--- app/lib/stores/editor.ts
+++ app/lib/stores/editor.ts
@@ -1,95 +1,44 @@
-import { atom, computed, map, type MapStore, type WritableAtom } from 'nanostores';
-import type { EditorDocument, ScrollPosition } from '~/components/editor/codemirror/CodeMirrorEditor';
-import type { FileMap, FilesStore } from './files';
+import { atom } from "nanostores";
+import { getPersistedItem, persistAtom } from "../persistence";
 
-export type EditorDocuments = Record<string, EditorDocument>;
+export type EditorState = {
+  activeFile?: {
+    path: string;
+    content: string;
+    mimeType: string;
+  };
+};
 
-type SelectedFile = WritableAtom<string | undefined>;
+const initial: EditorState = {};
 
-export class EditorStore {
-  #filesStore: FilesStore;
+export const $editor = atom<EditorState>(getPersistedItem("editor", initial));
 
-  selectedFile: SelectedFile = import.meta.hot?.data.selectedFile ?? atom<string | undefined>();
-  documents: MapStore<EditorDocuments> = import.meta.hot?.data.documents ?? map({});
-
-  currentDocument = computed([this.documents, this.selectedFile], (documents, selectedFile) => {
-    if (!selectedFile) {
-      return undefined;
-    }
-
-    return documents[selectedFile];
+export const openFile = (file: EditorState["activeFile"]) => {
+  $editor.set({
+    ...$editor.get(),
+    activeFile: file,
   });
+};
 
-  constructor(filesStore: FilesStore) {
-    this.#filesStore = filesStore;
+export const closeFile = () => {
+  $editor.set({
+    ...$editor.get(),
+    activeFile: undefined,
+  });
+};
 
-    if (import.meta.hot) {
-      import.meta.hot.data.documents = this.documents;
-      import.meta.hot.data.selectedFile = this.selectedFile;
-    }
-  }
-
-  setDocuments(files: FileMap) {
-    const previousDocuments = this.documents.value;
-
-    this.documents.set(
-      Object.fromEntries<EditorDocument>(
-        Object.entries(files)
-          .map(([filePath, dirent]) => {
-            if (dirent === undefined || dirent.type === 'folder') {
-              return undefined;
-            }
-
-            const previousDocument = previousDocuments?.[filePath];
-
-            return [
-              filePath,
-              {
-                value: dirent.content,
-                filePath,
-                scroll: previousDocument?.scroll,
-              },
-            ] as [string, EditorDocument];
-          })
-          .filter(Boolean) as Array<[string, EditorDocument]>,
-      ),
-    );
-  }
-
-  setSelectedFile(filePath: string | undefined) {
-    this.selectedFile.set(filePath);
-  }
-
-  updateScrollPosition(filePath: string, position: ScrollPosition) {
-    const documents = this.documents.get();
-    const documentState = documents[filePath];
-
-    if (!documentState) {
-      return;
-    }
-
-    this.documents.setKey(filePath, {
-      ...documentState,
-      scroll: position,
+export const updateFileContent = (content: string) => {
+  const activeFile = $editor.get().activeFile;
+  if (activeFile) {
+    $editor.set({
+      ...$editor.get(),
+      activeFile: {
+        ...activeFile,
+        content,
+      },
     });
   }
+};
 
-  updateFile(filePath: string, newContent: string) {
-    const documents = this.documents.get();
-    const documentState = documents[filePath];
-
-    if (!documentState) {
-      return;
-    }
-
-    const currentContent = documentState.value;
-    const contentChanged = currentContent !== newContent;
-
-    if (contentChanged) {
-      this.documents.setKey(filePath, {
-        ...documentState,
-        value: newContent,
-      });
-    }
-  }
-}
+persistAtom($editor, "editor");
+export type EditorStore = typeof $editor;
