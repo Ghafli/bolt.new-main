018c80ff96113df7c039e94470023779905ab46e
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-25 12:51:36 +0300

    Update markdown.ts

--- app/utils/markdown.ts
+++ app/utils/markdown.ts
@@ -1,116 +1,20 @@
-import rehypeRaw from 'rehype-raw';
-import remarkGfm from 'remark-gfm';
-import type { PluggableList, Plugin } from 'unified';
-import rehypeSanitize, { defaultSchema, type Options as RehypeSanitizeOptions } from 'rehype-sanitize';
-import { SKIP, visit } from 'unist-util-visit';
-import type { UnistNode, UnistParent } from 'node_modules/unist-util-visit/lib';
+import { marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
 
-export const allowedHTMLElements = [
-  'a',
-  'b',
-  'blockquote',
-  'br',
-  'code',
-  'dd',
-  'del',
-  'details',
-  'div',
-  'dl',
-  'dt',
-  'em',
-  'h1',
-  'h2',
-  'h3',
-  'h4',
-  'h5',
-  'h6',
-  'hr',
-  'i',
-  'ins',
-  'kbd',
-  'li',
-  'ol',
-  'p',
-  'pre',
-  'q',
-  'rp',
-  'rt',
-  'ruby',
-  's',
-  'samp',
-  'source',
-  'span',
-  'strike',
-  'strong',
-  'sub',
-  'summary',
-  'sup',
-  'table',
-  'tbody',
-  'td',
-  'tfoot',
-  'th',
-  'thead',
-  'tr',
-  'ul',
-  'var',
-];
+marked.use(
+  markedHighlight({
+    langPrefix: "hljs language-",
+    highlight(code, lang) {
+        if (!lang || lang === 'text') {
+            return code
+        }
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  })
+);
 
-const rehypeSanitizeOptions: RehypeSanitizeOptions = {
-  ...defaultSchema,
-  tagNames: allowedHTMLElements,
-  attributes: {
-    ...defaultSchema.attributes,
-    div: [...(defaultSchema.attributes?.div ?? []), 'data*', ['className', '__boltArtifact__']],
-  },
-  strip: [],
-};
-
-export function remarkPlugins(limitedMarkdown: boolean) {
-  const plugins: PluggableList = [remarkGfm];
-
-  if (limitedMarkdown) {
-    plugins.unshift(limitedMarkdownPlugin);
-  }
-
-  return plugins;
+export function markdown(text: string) {
+    return marked.parse(text);
 }
-
-export function rehypePlugins(html: boolean) {
-  const plugins: PluggableList = [];
-
-  if (html) {
-    plugins.push(rehypeRaw, [rehypeSanitize, rehypeSanitizeOptions]);
-  }
-
-  return plugins;
-}
-
-const limitedMarkdownPlugin: Plugin = () => {
-  return (tree, file) => {
-    const contents = file.toString();
-
-    visit(tree, (node: UnistNode, index, parent: UnistParent) => {
-      if (
-        index == null ||
-        ['paragraph', 'text', 'inlineCode', 'code', 'strong', 'emphasis'].includes(node.type) ||
-        !node.position
-      ) {
-        return true;
-      }
-
-      let value = contents.slice(node.position.start.offset, node.position.end.offset);
-
-      if (node.type === 'heading') {
-        value = `\n${value}`;
-      }
-
-      parent.children[index] = {
-        type: 'text',
-        value,
-      } as any;
-
-      return [SKIP, index] as const;
-    });
-  };
-};
