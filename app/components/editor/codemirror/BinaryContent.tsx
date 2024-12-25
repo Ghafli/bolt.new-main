a48c54f6f107487f46088238d8771c4452903971
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-25 11:31:07 +0300

    Update BinaryContent.tsx

--- app/components/editor/codemirror/BinaryContent.tsx
+++ app/components/editor/codemirror/BinaryContent.tsx
@@ -1,7 +1,28 @@
-export function BinaryContent() {
+import { FunctionComponent } from "react";
+
+export interface Props {
+  content: ArrayBuffer | null | undefined;
+  mimeType?: string;
+}
+
+const BinaryContent: FunctionComponent<Props> = ({ content, mimeType }) => {
+  if (!content) {
+    return null;
+  }
+
+  const blob = new Blob([content], { type: mimeType });
+  const url = URL.createObjectURL(blob);
+
   return (
-    <div className="flex items-center justify-center absolute inset-0 z-10 text-sm bg-tk-elements-app-backgroundColor text-tk-elements-app-textColor">
-      File format cannot be displayed.
+    <div className="flex flex-col items-center justify-center p-4">
+      <img
+        src={url}
+        alt="Binary Content"
+        style={{ maxWidth: "100%", maxHeight: "100%" }}
+        onLoad={() => URL.revokeObjectURL(url)}
+      />
     </div>
   );
-}
+};
+
+export default BinaryContent;
