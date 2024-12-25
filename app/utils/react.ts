7c906075764d0a9c42db7ae36c38f63e0a8a82a0
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-25 12:53:08 +0300

    Update react.ts

--- app/utils/react.ts
+++ app/utils/react.ts
@@ -1,6 +1,29 @@
-import { memo } from 'react';
+import {
+    useCallback,
+    useEffect,
+    useLayoutEffect,
+    useRef,
+    useState,
+} from "react";
 
-export const genericMemo: <T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>>(
-  component: T,
-  propsAreEqual?: (prevProps: React.ComponentProps<T>, nextProps: React.ComponentProps<T>) => boolean,
-) => T & { displayName?: string } = memo;
+export const useIsomorphicLayoutEffect =
+    typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+export function useSyncedState<T>(initialState: T) {
+    const [state, setState] = useState<T>(initialState);
+    const ref = useRef(state);
+
+    useEffect(() => {
+        ref.current = state;
+    }, [state]);
+
+    const setSyncedState = useCallback(
+        (newState: T) => {
+            setState(newState);
+            ref.current = newState;
+        },
+        [setState]
+    );
+
+    return [state, setSyncedState, ref] as const;
+}
