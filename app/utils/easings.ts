058d565c88a4837038d825136b0bed3bcd1d3a88
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-25 12:50:40 +0300

    Update easings.ts

--- app/utils/easings.ts
+++ app/utils/easings.ts
@@ -1,3 +1,9 @@
-import { cubicBezier } from 'framer-motion';
+// Easing functions
 
-export const cubicEasingFn = cubicBezier(0.4, 0, 0.2, 1);
+export const easeInOutQuad = (t: number): number => {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+};
+
+export const easeOutQuad = (t: number): number => {
+  return t * (2 - t);
+};
